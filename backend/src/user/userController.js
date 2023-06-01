cd161a5acf5621ac1b766002a7a0cf61fd9a3e81
ahmetaggoz <ahmet.aggoz19@ogr.atauni.edu.tr>
2023-06-02 00:34:17 +0300

    proje

--- backend/src/user/userController.js
+++ backend/src/user/userController.js
@@ -1,14 +1,15 @@
 const userService = require('./userService')
 
 const getUserControllerFn = async (req, res) =>{
-    let listings = await userService.getDataFromDBService();
-    res.send({ "status": true, "data": listings })
+    let data = await userService.getDataFromDBService();
+    res.send({ "status": true, "data": data })
 }
 
-const getCountriesControllerFn = async (req, res) => {
-    let countries = await userService.getCountriesFromDBService(req.body);
-    res.send({ "status": true, "data": countries})
+const getCountriesControllerFn = async (req, res) =>{
+    let data = await userService.getCountriesFromDBService(req.body);
+    res.send({ "status": true, "data": data })
 }
+
 
 
 module.exports = {getUserControllerFn, getCountriesControllerFn}
--- backend/src/user/userService.js
+++ backend/src/user/userService.js
@@ -11,27 +11,26 @@ module.exports.getDataFromDBService = () =>{
     })
 }
 
-module.exports.getCountriesFromDBService = (regionName) =>{
-    return new Promise(function checkURL(resolve, reject){
+module.exports.getCountriesFromDBService = (regionName="")=>{
+    return new Promise(function (resolve, reject){
         try {
-            const region = userModel.regions.find({name: regionName.name}).toArray()
-            region.then(datalarim=>{
-                const countries = userModel.listings.find({
+            let myList = []
+            let result = userModel.regions.find({name: regionName.name}).toArray()
+            result.then((hamVeri) => myList.push(hamVeri)).then(()=> {
+                let result2 = userModel.listings.find({
                     'address.location': {
                         '$geoWithin': {
                             '$geometry': {
-                                'type': datalarim[0].geometry.type,
-                                'coordinates': datalarim[0].geometry.coordinates
+                                'type': myList[0][0]['geometry']['type'],
+                                'coordinates': myList[0][0]['geometry']['coordinates']
                             }
                         }
                     }
                 }).toArray()
-                resolve(countries)
-                console.log(countries)
+                resolve(result2)
             })
+
         } catch (error) {
             reject(false)
         }
-    })
-}
-
+})}
